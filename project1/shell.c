e90d965c6111ce67af123be683574a172a825258
Laura Joy Erb <laurajoy@bu.edu>
2019-09-16 15:30:08 -0400

    parse string tokens

--- project1/shell.c
+++ project1/shell.c
@@ -2,16 +2,33 @@
 #include <stdbool.h>
 #include <libc.h>
 
+#define MAX_INPUT 1024
+#define MAX_TOKENS 200
+
 int main(int argc, char* argv[]) {
   bool print = true;
   if (argc > 1) {
     print = (strcmp(argv[1],"-n") != 0);
   }
+  int tokenCount = 0;
+  char input[MAX_INPUT];
+  char* tokens[MAX_TOKENS];
+  char* ptr = NULL;
+
   while (1) {
-    char input[1000];
     if (print) { printf("my_shell$ "); }
-    fgets(input, 1000, stdin);
-    printf("%s", input);
+    fgets(input, MAX_INPUT, stdin);
+    tokenCount = 0;
+    ptr = strtok(input, " ");
+    while (ptr != NULL) {
+      tokens[tokenCount] = ptr;
+      tokenCount++;
+      ptr = strtok(NULL, " ");
+    }
+
+    for (int i = 0; i < tokenCount; i++) {
+      printf("%s\n", tokens[i]);
+    }
   }
   return 0;
 }
