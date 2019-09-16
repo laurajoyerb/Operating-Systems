bf1e308a00aa7eeb7a378f671118bb669c3b8269
Laura Joy Erb <laurajoy@bu.edu>
2019-09-15 23:44:48 -0400

    amended -n arg for no printing

--- project1/shell.c
+++ project1/shell.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
+#include <libc.h>
 
 int main(int argc, char* argv[]) {
-  bool print = (strcmp(argv[1],"-n") != 0);
+  bool print = true;
+  if (argc > 1) {
+    print = (strcmp(argv[1],"-n") != 0);
+  }
   while (1) {
     char input[1000];
     if (print) { printf("my_shell$ "); }
