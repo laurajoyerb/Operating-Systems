e3e33d8647f1229c6f7fc0a061772389d59410e1
Laura Joy Erb <laurajoy@bu.edu>
2019-10-08 19:49:50 -0400

    added sample main and makefile
    also header
    does compile on server

--- project2/threads.c
+++ project2/threads.c
@@ -3,6 +3,15 @@
 #include<pthread.h>
 #include <stdlib.h>
 
+// struct thread {
+// 	pthread_t id;
+// 	// Information about the state of the thread (set of registers)
+//
+// 	// Information about its stack (pointer to stack area)
+// 	Information about status of thread (ready, running, exited)
+// }
+
+
 int pthread_create(
   pthread_t *thread,
   const pthread_attr_t *attr,
@@ -19,8 +28,8 @@ pthread_t pthread_self(void) {
   pthread_t foo = NULL;
   return foo;
 }
-
-int main()
-{
-    return 0;
-}
+// 
+// int main()
+// {
+//     return 0;
+// }
