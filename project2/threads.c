24a78e90e5b48ae4385cbb7e9276914dd28edf50
Laura Joy Erb <laurajoy@bu.edu>
2019-10-14 22:44:15 -0400

    initialize func, create thread
    not tested yet

--- project2/threads.c
+++ project2/threads.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <setjmp.h>
 #include <stdbool.h>
+#include "ec440threads.h"
 
 #define MAX_THREADS 128
 
@@ -12,7 +13,18 @@
 #define RUNNING 1
 #define EXITED 2
 
+// registers of jump buf
+#define JB_RBX 0
+#define JB_RBP 1
+#define JB_R12 2
+#define JB_R13 3
+#define JB_R14 4
+#define JB_R15 5
+#define JB_RSP 6
+#define JB_PC 7
+
 bool initialized = false;
+int activeThreads = 0;
 
 struct thread {
 	pthread_t id;
@@ -26,7 +38,12 @@ struct thread processThreads[MAX_THREADS];
 
 void initialize() {
 	initialized = true;
-	printf("intitializing\n");
+
+	processThreads[0].id = 0;
+	processThreads[0].state = READY;
+	processThreads[0].rsp = NULL;
+	setjmp(processThreads[0].reg);
+	activeThreads++;
 }
 
 int pthread_create(
@@ -37,6 +54,21 @@ int pthread_create(
 		if (!initialized) {
 			initialize();
 		}
+
+		if (activeThreads < MAX_THREADS) {
+			processThreads[activeThreads].id = *thread;
+			processThreads[activeThreads].state = READY;
+			processThreads[activeThreads].rsp = malloc(32767);
+			setjmp(processThreads[activeThreads].reg);
+
+			// manually set start routine
+			processThreads[activeThreads].reg[JB_PC] = ptr_mangle((unsigned long) start_thunk);
+			processThreads[activeThreads].reg[JB_R13] = ptr_mangle((unsigned long) arg);
+			processThreads[activeThreads].reg[JB_R12] = ptr_mangle((unsigned long) start_routine);
+
+			activeThreads++;
+		}
+
     return 0;
 }
 
