7172658fb26c9b612d9ffe7ce97263ed1afd3cf7
Laura Joy Erb <laurajoy@bu.edu>
2019-11-19 17:22:22 -0500

    added tls init

--- project4/tls.c
+++ project4/tls.c
@@ -1,8 +1,13 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 #include "semaphore.h"
 #include "tls.h"
+#include <stdbool.h>
+#include <signal.h>
+
+#define HASH_SIZE 100
 
 struct page {
   unsigned int address;
@@ -15,6 +20,36 @@ typedef struct thread_local_storage {
   unsigned int num_ages;
   struct page ** pages;
 } TLS;
+
+struct hash_element {
+  pthread_t id;
+  TLS *tls;
+  struct hash_element *next;
+};
+
+bool initialized = false;
+int page_size;
+
+struct hash_element* hash_table[HASH_SIZE];
+
+void tls_handle_page_fault() {
+  printf("page fault handled\n");
+}
+
+void tls_init() {
+  struct sigaction sigact;
+
+  page_size = getpagesize();
+
+  sigemptyset(&sigact.sa_mask);
+  sigact.sa_flags = SA_SIGINFO;
+  sigact.sa_sigaction = tls_handle_page_fault;
+
+  sigaction(SIGBUS, &sigact, NULL);
+  sigaction(SIGSEGV, &sigact, NULL);
+
+  initialized = true;
+}
 
 int tls_create(unsigned int size) {
   return 0;
